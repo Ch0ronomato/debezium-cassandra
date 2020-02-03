30431e05c04d9f3145fd0c32f355ed64381efbcf
Ian Schweer <ian.schweer@doordash.com>
2020-02-03 10:56:43 -0800

    Add debezium logs

--- start-debezium.sh
+++ start-debezium.sh
@@ -1,1 +1,1 @@
-java -cp "debezium-connector-cassandra/*" io.debezium.connector.cassandra.CassandraConnectorTask /etc/debezium/cdc.properties
+java -cp "debezium-connector-cassandra/*" io.debezium.connector.cassandra.CassandraConnectorTask /etc/debezium/cdc.properties > debezium.stdout.log 2> debezium.stderr.log
