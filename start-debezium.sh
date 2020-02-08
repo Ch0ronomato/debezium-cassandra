d125a779f32d747748410ebf5d9d5fc1a0b5e0aa
Ian Schweer <ian.schweer@doordash.com>
2020-02-07 18:40:51 -0800

    Switch to jar

--- start-debezium.sh
+++ start-debezium.sh
@@ -1,1 +1,1 @@
-java -cp "debezium-connector-cassandra/*" io.debezium.connector.cassandra.CassandraConnectorTask /etc/debezium/cdc.properties > debezium.stdout.log 2> debezium.stderr.log
+java -jar debezium-connector-cassandra-1.1.0.Beta1-jar-with-dependencies.jar /etc/debezium/cdc.properties > debezium.stdout.log 2> debezium.stderr.log
